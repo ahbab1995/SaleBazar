c3db707f4e6da0aa9004d9893ce800222f46917d
ahbab1995 <islamahbab1996@yahoo.com>
2023-03-27 01:23:17 +0600

    Product Upload  successfully

--- src/components/UploadProduct/UploadProduct.js
+++ src/components/UploadProduct/UploadProduct.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from "react";
+import { Button, Form } from "react-bootstrap";
 
 const UploadProduct = () => {
-  return (
-    <div>
-        <h1>UploadProduct</h1>
-    </div>
-  )
-}
 
-export default UploadProduct
+  const handleUpload = (e) =>{
+    e.preventDefault();
+    const name = e.target.name.value
+     const price = e.target.price.value
+    
+
+     const url = 'http://localhost:5000/uploadpd';
+
+     fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+       name,price
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        e.target.reset();
+      });
+  }
+
+  return (
+    <div >
+      <h1 className="text-center my-3">UploadProduct</h1>
+      <div className="w-50 mx-auto">
+        <Form onSubmit={handleUpload}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Product Name</Form.Label>
+            <Form.Control type="text" name="name" placeholder="Product Name" />           
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Price</Form.Label>
+            <Form.Control type="text" name="price" placeholder="Price" />
+          </Form.Group>
+          
+          <Button variant="primary" type="submit">
+            Upload
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default UploadProduct;
