25bdf91fcd04ec53f3df344e28068ca7b13196fd
ahbab1995 <islamahbab1996@yahoo.com>
2023-04-05 23:49:57 +0600

    project done

--- src/components/Orderlist/Orderlist.js
+++ src/components/Orderlist/Orderlist.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../firebase/firebase";
 
 const Orderlist = () => {
+  const [user, loading, error] = useAuthState(auth);
+  const [orderList, setOrderList] = useState([]);
+  useEffect(() => {
+    const url = `http://localhost:5000/orderlist`;
+    fetch(url, {
+      headers: {
+        authorization: `${user.email} ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setOrderList(data));
+  }, [user.email]);
+
   return (
     <div>
-        <h1>Order List</h1>
+      <h1>Order List {orderList.length}</h1>
+      <ol>
+        {
+          orderList.map(order=><li>{order.name}</li>)
+        }
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default Orderlist
+export default Orderlist;
